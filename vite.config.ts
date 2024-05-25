c15f85c32a11588a1cc1ba12122a4899411023f9
周周 <1989394260@qq.com>
2024-05-25 18:25:19 +0800

    fix: 导入elementui-plus和sass

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    VueSetupExtend()
   ],
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData:`@import "@/bem.scss";`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
